f0af2ececab4224437484762880cb5ee4005ab0f
rokujyushi <rosabu376@gmail.com>
2026-02-24 02:17:51 +0900

    obj/binフォルダーをクリーンアップする処理を追加

--- build_release.ps1
+++ build_release.ps1
@@ -4,6 +4,10 @@ Push-Location $PSScriptRoot
 & "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Launch-VsDevShell.ps1" -Arch amd64
 Set-Location $PSScriptRoot
 
+# Clean obj/bin folders
+if (Test-Path ".\obj") { Remove-Item ".\obj" -Recurse -Force }
+if (Test-Path ".\bin") { Remove-Item ".\bin" -Recurse -Force }
+
 # Build VariableFont
 msbuild ".\VariableFont.vcxproj" /p:Configuration=Release /p:Platform=x64
 
